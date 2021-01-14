112e69b64e90b0e18b629e4c16f53344417540ed
usabarashi <19676305+usabarashi@users.noreply.github.com>
2021-01-14 17:07:11 +0000

    Add setup.py.

---
+++ setup.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+from setuptools import find_packages, setup
+
+with open("README.md") as file:
+    readme = file.read()
+
+with open("LICENSE") as file:
+    license = file.read()
+
+
+setup(
+    name="category",
+    version="0.1.0",
+    description="Provide a functional programming library",
+    long_description=readme,
+    author="usabarashi",
+    author_email="",
+    url="",
+    license=license,
+    packages=find_packages(exclude=("tests", "docs")),
+    install_requires=[],
+)
