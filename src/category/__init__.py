2dc32752954dd137825fdd4a5ff54c23de7ae16a
usabarashi <19676305+usabarashi@users.noreply.github.com>
2021-03-07 18:54:19 +0000

    Update the library path.

--- src/category/__init__.py
+++ src/category/__init__.py
@@ -1,2 +1,13 @@
 from .collection import Vector
-from .monad import Either, Failure, Future, Left, Right, Success, Try
+from .either import (
+    Either,
+    EitherDo,
+    EitherError,
+    EitherST,
+    Left,
+    LeftProjection,
+    Right,
+    RightProjection,
+)
+from .option import Option, OptionDo, OptionError, OptionST, Some, Void
+from .try_ import Failure, Success, Try, TryDo, TryError, TryST
