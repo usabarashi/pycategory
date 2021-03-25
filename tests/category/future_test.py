cfd1add6632a4175608839fb7527a765f3d18212
usabarashi <19676305+usabarashi@users.noreply.github.com>
2021-03-25 21:52:06 +0000

    WIP Modified to Python based Future.

--- tests/category/future_test.py
+++ tests/category/future_test.py
@@ -1,28 +1,49 @@
-def test_future():
-    from category import Failure, Future, Success, TryST
+def test_map():
 
-    def add_one(try_: TryST[int]) -> TryST[int]:
+    from category import Future
+    from category import ThreadPoolExecutionContext as ec
+
+    success_future = Future.successful(1)
+    mapped_future = success_future.map(functor=lambda success: success + 1, ec=ec)
+    assert success_future is not mapped_future
+    assert isinstance(mapped_future, Future)
+    assert 2 == mapped_future.result()
+
+
+def test_try_complete():
+    from category import Future, Success
+
+    true_future = Future.successful(1)
+    assert False is true_future.try_complete(result=Success(value=0))
+    assert 1 == true_future.result()
+
+
+def test_on_complete():
+    from typing import TypeVar, Union
+
+    from category import Failure, Future, ThreadPoolExecutionContext, TryST
+
+    T = TypeVar("T")
+
+    def functor(try_: TryST[T]) -> Union[Exception, int]:
         if isinstance(try_, Failure):
-            return Success(value=0)
+            return try_.value
         else:
-            return Success(value=try_.value + 1)
+            return try_.get_or_else(0) + 1
 
-    def add_two_flat(try_: TryST[int]) -> Future[int]:
-        if isinstance(try_, Failure):
-            return Future.successful(value=0)
-        else:
-            return Future.successful(value=try_.value + 2)
+    true_future = Future.successful(1)
+    true_future.on_complete(
+        functor=functor,
+        ec=ThreadPoolExecutionContext,
+    )
+    assert 2 == true_future.result()
 
-    assert Future is type(Future[int].successful(1).transform(add_one))
-    assert 2 == Future[int].successful(1).transform(add_one).value.value
-    assert Future is type(
-        Future[int].successful(1).transform(add_one).transform_with(add_two_flat)
-    )
-    assert (
-        4
-        == Future[int]
-        .successful(1)
-        .transform(add_one)
-        .transform_with(add_two_flat)
-        .value.value
-    )
+
+def test_successful():
+    from category import Future, Success
+
+    future = Future.successful(True)
+    assert Success(value=True) == future.value
+    assert True is future.value.value
+
+    assert True is future.result()
