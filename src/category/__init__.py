1cb11683faba64491816bbbec74e4fb57dcbb4c1
usabarashi <19676305+usabarashi@users.noreply.github.com>
2022-06-10 00:02:27 +0000

    Add SINGLETON_VOID

--- src/category/__init__.py
+++ src/category/__init__.py
@@ -2,6 +2,7 @@ from .collection import Vector
 from .either import Either, EitherDo, Left, LeftProjection, Right, RightProjection
 from .eithert import EitherTFuture, EitherTFutureDo, EitherTTry, EitherTTryDo
 from .future import ExecutionContext, Future, FutureDo, ThreadPoolExecutionContext
+from .option import SINGLETON_VOID as VOID
 from .option import Option, OptionDo, Some, Void
 from .try_ import Failure, Success, Try, TryDo
 
@@ -25,6 +26,7 @@ __all__ = [
     "OptionDo",
     "Some",
     "Void",
+    "VOID",
     "Failure",
     "Success",
     "Try",
