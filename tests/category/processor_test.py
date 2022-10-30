bf716ca2ba09dda31059861a850ef5858c7015a6
usabarashi <usabarashi@mac.com>
2022-10-30 09:12:56 +0000

    Add Either case.

--- tests/category/processor_test.py
+++ tests/category/processor_test.py
@@ -29,14 +29,14 @@ def test_arguments():
         ...
 
     args = (0, 1, "1")
-    kwargs = {"name2": [2], "name3": {3: 3}}
+    kwargs = {"name2": ["2"], "name3": {3: 3}}
 
     arguments = processor.arguments(annotations_arguments, *args, **kwargs)
     assert {
         "position1": 0,
         "position2": 1,
         "name1": "1",
-        "name2": [2],
+        "name2": ["2"],
         "name3": {3: 3},
     } == arguments
 
@@ -54,7 +54,7 @@ def test_arguments():
         "position1": 0,
         "position2": 1,
         "name1": "1",
-        "name2": [2],
+        "name2": ["2"],
         "name3": {3: 3},
     } == arguments
 
@@ -72,7 +72,7 @@ def test_arguments():
         "position1": 0,
         "position2": 1,
         "name1": "1",
-        "name2": [2],
+        "name2": ["2"],
         "name3": {3: 3},
     } == arguments
 
@@ -91,7 +91,7 @@ def test_arguments():
 
 
 def test_frame():
-    from category import Frame, processor
+    from category import Either, EitherDo, Frame, Left, Right, processor
 
     def function(arg1: int, arg2: int, arg3: int) -> Frame:
         variable1 = 42
@@ -106,3 +106,19 @@ def test_frame():
     assert 42 == frame.variables.get("variable1", None)
     assert processor.MASK == frame.variables.get("variable2", None)
     assert processor.MASK == frame.variables.get("variable3", None)
+
+    class Error(Frame):
+        ...
+
+    @Either.do
+    def context(*, mask: int, unmask: int) -> EitherDo[Error, int]:
+        one = yield from Left[Error, int](Error(unmask=("unmask",)))
+        two = 2
+        three = yield from Right[Error, int](3)
+        return one + two + three
+
+    result = context(mask=42, unmask=42)
+    assert Left is type(result)
+    assert Error is type(result.left().get())
+    assert "****" == result.left().get().variables.get("mask", None)
+    assert 42 == result.left().get().variables.get("unmask", None)
