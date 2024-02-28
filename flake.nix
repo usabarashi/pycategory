6b644cfcf5499bfe7d0f5a953170cfe68af15b3a
usabarashi <19676305+usabarashi@users.noreply.github.com>
2024-02-29 02:22:03 +0900

    Add .envrc, .direnv, flake.nix, and flake.lock files

---
+++ flake.nix
@@ -0,0 +1,18 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/23.11";
+  };
+
+  outputs = inputs:
+    let
+      pkgs = import inputs.nixpkgs { system = "aarch64-darwin"; };
+    in
+    {
+      devShells."aarch64-darwin".default = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          poetry
+          python310
+        ];
+      };
+    };
+}
